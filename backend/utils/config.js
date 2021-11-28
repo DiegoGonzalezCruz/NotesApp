ff49a472b988a06ec2bb4776deb7401b0682ea9e
DiegoGonzalezCruz <diego@thinkey.cl>
2021-11-28 11:32:29 -0300

    supertest

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,11 +1,18 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
+
+const MONGODB_URI_LOCAL = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI_LOCAL
+
+
+/* 
 const MONGODB_URI = process.env.MONGODB_URI
-const MONGODB_URI_LOCAL = process.env.MONGODB_URI_LOCAL
+const MONGODB_URI_LOCAL = process.env.MONGODB_URI_LOCAL 
+*/
 
 module.exports = {
-    MONGODB_URI,
     MONGODB_URI_LOCAL,
     PORT
 }
